004e1d511f13aee508f340b2b4a0c5cb0f7425ff
Ganesh <ganeshudmale02@gmal.com>
2025-08-07 22:42:31 +0530

    Day 4 statelockig

--- Day-4-statelocking/main.tf
+++ Day-4-statelocking/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
   ami = "ami-0d54604676873b4ec"
-  instance_type = "t2.micro"
+  instance_type = "t2.nano"
   subnet_id = "subnet-0492674c95b731151"
   tags = {
     Name ="developer"
