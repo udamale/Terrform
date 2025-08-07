1c124d5c8475060e4759b2617017d1e12248c7ba
Ganesh <ganeshudmale02@gmal.com>
2025-08-07 12:51:46 +0530

    Day 4 statelockin

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -3,5 +3,8 @@ terraform {
     bucket = "udamale.xyz"
     key = "Day 4/terraform.tfstate"
     region = "ap-south-1"
+     #use_lockfile = true #s3 supports this feature but teraaform version > 1.10, latest version >=1.10
+     dynamodb_table = "test"  #any version 
+    encrypt = true
   }
 }
--- Day-4-statelocking/main.tf
+++ Day-4-statelocking/main.tf
@@ -1,11 +1,11 @@
 resource "aws_instance" "name" {
   ami = "ami-0d54604676873b4ec"
-  instance_type = "t2.micro"
+  instance_type = "t2.micor"
   subnet_id = "subnet-0492674c95b731151"
   tags = {
-    Name ="devloper"
+    Name ="test"
   }
-
+git 
 }
 
 resource "aws_vpc" "name" {
