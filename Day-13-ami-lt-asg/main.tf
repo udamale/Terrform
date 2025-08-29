f8b6fae50599c144eb7e4dc7dc3f9259b29dfbad
Ganesh <ganeshudmale02@gmal.com>
2025-08-29 19:20:17 +0530

    Day-13-ami-lt-asg

--- Day-13-ami-lt-asg/main.tf
+++ Day-13-ami-lt-asg/main.tf
@@ -1,17 +1,17 @@
 provider "aws" {
   
 }
-
+# data source block of public subnet
 data "aws_subnet" "name" {
   filter {
-    name = "tag:name"
+    name = "tag:Name"
     values = [ "public_sub" ]
   }
 }
 
 data "aws_subnet" "private" {
     filter {
-      name = "tag:name"
+      name = "tag:Name"
       values = [ "public-subnet-1-1a" ]
     }
   
@@ -26,11 +26,51 @@ data "aws_ami" "name" {
   }
 }
 
+data "aws_vpc" "name" {
+  tags = {
+    Name= "main_vpc"
+  }
+}
+
+# creation of security group with same cidr
+resource "aws_security_group" "name" {
+    vpc_id = data.aws_vpc.name.id
+  name = "terraform-project"
+  description = "Allow TLS inbound traffic"
+
+        ingress = [
+        for port in [22, 80, 443, 9000, 3000, 8082, 8081 ,8000] : {
+        description      = "inbound rules"
+        from_port        = port
+        to_port          = port
+        protocol         = "tcp"
+        cidr_blocks      = ["0.0.0.0/0"]
+        ipv6_cidr_blocks=[]
+        prefix_list_ids  = []
+        security_groups  = []
+        self             = false
+        }
+    ]
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  tags = {
+    Name = "terraform-project"
+  }
+}
+
+#creation of Ec2 server
 resource "aws_instance" "server" {
   ami = data.aws_ami.name.id
-  subnet_id = data.aws_ami.name.id
+  subnet_id = data.aws_subnet.name.id
   instance_type = "t2.micro"
-  associate_public_ip_address = true
+  vpc_security_group_ids = [ aws_security_group.name.id ]
+#  associate_public_ip_address = true
 
   tags = {
     Name= "web server"
@@ -38,23 +78,88 @@ resource "aws_instance" "server" {
   user_data = file("test.sh")
 }
 
+# creation of the target group 
+resource "aws_lb_target_group" "name" {
+  name = "tg-server"
+  port = "80"
+  protocol = "HTTP"
+  vpc_id =data.aws_vpc.name.id
+
+   target_group_health {
+    dns_failover {
+      minimum_healthy_targets_count = "1"
+      minimum_healthy_targets_percentage = "off"
+    }
+    unhealthy_state_routing {
+      minimum_healthy_targets_count = "1"
+      minimum_healthy_targets_percentage = "off"
+    }
+  }  
+}
+
+# target group attechment to target instace ec2 server   
+resource "aws_lb_target_group_attachment" "name" {
+  target_group_arn = aws_lb_target_group.name.arn
+  target_id = aws_instance.server.id
+  port = 80 
+}
+
+resource "aws_lb" "name" {
+    depends_on = [ aws_lb_target_group.name ]
+  name = "server-alb"
+  internal = false
+  load_balancer_type = "application"
+ security_groups = [ aws_security_group.name.id ]
+subnets = concat(
+  [data.aws_subnet.name.id],
+  [data.aws_subnet.private.id]
+)
+
+#  access_logs {
+#     bucket  = aws_s3_bucket.lb_logs.id
+#     prefix  = "test-lb"
+#     enabled = true
+#   }
+}
+
+resource "aws_lb_listener" "web_server" {
+    depends_on = [ aws_lb.name ]
+    load_balancer_arn = aws_lb.name.arn
+    port = "80"
+    protocol = "HTTP"
+
+     default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.name.arn
+  }
+  
+}
+
+output "load_balancer_dns_name" {
+  description = "The DNS name of the ALB"
+  value       = aws_lb.name.dns_name
+}
+
+# creation of ami image form instance
 resource "aws_ami_from_instance" "name" {
+  depends_on = [ aws_instance.server ]
     source_instance_id = aws_instance.server.id
     name = "web server"
     snapshot_without_reboot = true
   
 }
 
-
+# creation of lunche template and attecha to the ami image
 resource "aws_launch_template" "web_lt" {
   name_prefix   = "web-lt-"
   image_id      = aws_ami_from_instance.name.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.name.id ]
 
-  network_interfaces {
-    subnet_id              = data.aws_subnet.name.id
-    associate_public_ip_address = true
-  }
+  # network_interfaces {
+  #   subnet_id              = data.aws_subnet.name.id
+  #   associate_public_ip_address = true
+  # }
 
   user_data = filebase64("test.sh")
 
@@ -66,11 +171,13 @@ resource "aws_launch_template" "web_lt" {
   }
 }
 
+
+# cretion of asg server and attach to the lunche templet
 resource "aws_autoscaling_group" "asg" {
   name_prefix          = "my-asg-"
   min_size             = 1
-  max_size             = 3
-  desired_capacity     = 2
+  max_size             = 2
+  desired_capacity     = 1
 
   # Specify the Launch Template to use
   launch_template {
@@ -81,25 +188,26 @@ resource "aws_autoscaling_group" "asg" {
   # Subnets for scaling instances
   vpc_zone_identifier = [
     data.aws_subnet.name.id,
+    data.aws_subnet.private.id
     # Add more subnet IDs as needed
   ]
 
   # Optional: ALB integration for load balancing
   target_group_arns = [
-    aws_lb_target_group.my_alb_tg.arn,
+    aws_lb_target_group.name.arn
   ]
 
   health_check_type         = "EC2"
-  health_check_grace_period = 300
+  health_check_grace_period = 100
 
   # Optional: Rolling updates when the template is updated
   instance_refresh {
     strategy = "Rolling"
     preferences {
       min_healthy_percentage = 50
-      instance_warmup        = 300
+      instance_warmup        = 200
     }
-    triggers = ["launch_template"]
+   # triggers = ["launch_template"]          // launch_template' always triggers an instance refres
   }
 
   tag {
