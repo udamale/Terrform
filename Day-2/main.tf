33e43621b7feb8c2c2128a23be83623d91e7ab58
Ganesh udamale <148082412+udamale@users.noreply.github.com>
2025-09-17 00:12:44 +0530

    Update main.tf

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "name" {
     ami = "ami-0b982602dbb32c5bd"
     instance_type = "t2.micro"
-    subnet_id = "subnet-0492674c95b7311511"
+    subnet_id = "subnet-0492674c95b731151"
   
 }
